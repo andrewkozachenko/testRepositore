937fa73a8594eabe572021134096345a248d3df4
Andrew_Kozachenko <andrew.kozachenko@gmail.com>
2014-01-26 20:12:08 +0200

    remove project

--- bookAgolf111/bookAgolf/src/com/academysmart/bookagolf/config/ColorRegistryKeys.java
+++
@@ -1,6 +0,0 @@
-package com.academysmart.bookagolf.config;
-
-public class ColorRegistryKeys {
-	public static String ODD_ROW = "com.academysmart.bookagolf.colors.oddrow";
-	public static String EVEN_ROW = "com.academysmart.bookagolf.colors.evenrow";
-}
