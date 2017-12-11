8c2491ca22199ed589e5bb33320b5075bc9ed9b2
Andrew Kozacheko <andrew.kozachenko@gmail.com>
2017-12-11 02:06:31 +0200

    Create student.java

---
+++ js-in-art-test-projects/student.java
@@ -0,0 +1,10 @@
+public class Student implements Serializable
+{
+private String name; 
+private String surname; 
+public Student() {}
+public String getName() {...} 
+public void setName(String n) {...} 
+public String getSername() {...}
+public void setSurname(String n) {...}
+}
